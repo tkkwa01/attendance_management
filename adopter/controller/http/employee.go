5dd17891e2845dfd55eb4c5b0a169778e24dad59
Takakuwa Aoi <aoi@TakakuwanoMacBook-Air.local>
2023-08-04 17:59:03 +0900

    controllerとpresenterとlogを書いた

---
+++ adopter/controller/http/employee.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"attendance-management/adopter/controller/presenter"
+	"attendance-management/packages/context"
+	"attendance-management/packages/http/router"
+	"attendance-management/resource/request"
+	"attendance-management/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+type employee struct {
+	inputFactory  usecase.EmployeeInputFactory
+	outputFactory func(c *gin.Context) usecase.EmployeeOutputPort
+}
+
+func NewEmployee(r *router.Router, inputFactory usecase.EmployeeInputFactory, outputFactory presenter.EmployeeOutputFactory) {
+	handler := employee{
+		inputFactory:  inputFactory,
+		outputFactory: outputFactory,
+	}
+
+	r.Group("employee", nil, func(r *router.Router) {
+		r.Post("", handler.Create)
+	})
+}
+
+func (e employee) Create(ctx context.Context, c *gin.Context) error {
+	var req request.CreateEmployee
+
+	if !bind(c, &req) {
+		return nil
+	}
+
+	outputPort := e.outputFactory(c)
+	inputPort := e.inputFactory(outputPort)
+
+	return inputPort.Create(ctx, &req)
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.20
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.0
+	github.com/google/uuid v1.3.0
+	go.uber.org/zap v1.25.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
 )
@@ -29,6 +31,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
